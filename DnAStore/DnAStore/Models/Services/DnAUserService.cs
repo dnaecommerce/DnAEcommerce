26d54fff5d4d529e108972c825f0976e0c7efedc
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-24 11:01:50 -0700

    Managers and Services created for products and users

---
+++ DnAStore/DnAStore/Models/Services/DnAUserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnAStore.Models.Services
+{
+    public class DnAUserService
+    {
+    }
+}
