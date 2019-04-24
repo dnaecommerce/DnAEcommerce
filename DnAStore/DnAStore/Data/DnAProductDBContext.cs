7136b0119114ee5117d9c295fe71f324906df531
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-24 10:34:07 -0700

    Built DBContext for Identity, built User model for Identity

---
+++ DnAStore/DnAStore/Data/DnAProductDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnAStore.Data
+{
+    public class DnAProductDBContext : DbContext
+    {
+
+        public DnAProductDBContext(DbContextOptions<DnAProductDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        // Database Tables
+
+
+    }
+}
---
+++ DnAStore/DnAStore/Data/DnAUserDBContext.cs
@@ -0,0 +1,20 @@
+﻿using DnAStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnAStore.Data
+{
+    public class DnAUserDBContext : IdentityDbContext<DnAUser>
+    {
+
+        public DnAUserDBContext(DbContextOptions<DnAUserDBContext> options) : base(options)
+        {
+
+        }
+        
+    }
+}
