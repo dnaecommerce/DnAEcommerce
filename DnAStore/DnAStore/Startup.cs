d68a0f25fe7cc9d8412d0661bd81cf42389ddf86
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-24 10:34:09 -0700

    Add connection strings for DBs, start adding Identity

--- DnAStore/DnAStore/Startup.cs
+++ DnAStore/DnAStore/Startup.cs
@@ -6,17 +6,42 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using DnAStore.Models;
 
 namespace DnAStore
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+		public IConfiguration Configuration { get; }
+		public IHostingEnvironment Environment { get; }
+		
+		public Startup(IHostingEnvironment environment)
+		{
+			Environment = environment;
+			var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+			builder.AddUserSecrets<Startup>();
+			Configuration = builder.Build();
+		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-        }
+
+			var connectionString_ProductsDB = Environment.IsDevelopment() ? Configuration["ConnectionStrings:ProductionConnection"] : Configuration["ConnectionStrings:ProductionConfiguration"];
+			var connectionString_UserDB = Environment.IsDevelopment() ? Configuration["ConnectionStrings:ProductionConnection"] : Configuration["ConnectionStrings:ProductionConnection"];
+
+			services.AddDbContext<DnAUserDbContext>(options => options.UseSqlServer(connectionString_UserDB));
+			services.AddDbContext<DnAProductDbContext>(options => options.UseSqlServer(connectionString_ProductsDB));
+
+			services.AddIdentity<DnAUser, IdentityRole>()
+				.AddEntityFrameworkStores<DnAUserDbContext>()
+				.AddDefaultTokenProviders();
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -26,6 +51,8 @@ namespace DnAStore
                 app.UseDeveloperExceptionPage();
             }
 
+			app.UseStaticFiles();
+			app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
