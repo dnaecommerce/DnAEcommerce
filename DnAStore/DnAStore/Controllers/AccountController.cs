6c6cd0750038ae5c8194f8017b73936e7a46ed27
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-25 10:44:44 -0700

    Add GET and POST methods for Register in AccountController

--- DnAStore/DnAStore/Controllers/AccountController.cs
+++ DnAStore/DnAStore/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using DnAStore.Models;
+using DnAStore.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,10 +30,36 @@ namespace DnAStore.Controllers
 		[HttpPost]
 		public async Task<IActionResult> Register(RegisterViewModel rvm)
 		{
-			if (true)
+			if (ModelState.IsValid)
 			{
+				User user = new User
+				{
+					UserName = rvm.Email,
+					Email = rvm.Email,
+					FirstName = rvm.FirstName,
+					LastName = rvm.LastName,
+				};
 
+				var result = await _userManager.CreateAsync(user, rvm.Password);
+
+				if (result.Succeeded)
+				{
+					Claim nameClaim = new Claim("FullName", $"{user.FirstName} {user.LastName}");
+
+					Claim emailClaim = new Claim(ClaimTypes.Email, user.Email, ClaimValueTypes.Email);
+
+					List<Claim> claims = new List<Claim> { nameClaim, emailClaim };
+
+					await _userManager.AddClaimsAsync(user, claims);
+
+					// Sign user in
+					await _signInManager.SignInAsync(user, isPersistent: false);
+
+					// Redirect to home page
+					return RedirectToAction("Index", "Home");
+				}
 			}
+			return View(rvm);
 		}
 	}
 }
