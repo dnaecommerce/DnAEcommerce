73ccd5dc469bd2e2840aafeac1b0971c26abf295
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-25 10:20:58 -0700

    Add RegisterViewModel and start on AccountController

---
+++ DnAStore/DnAStore/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DnAStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DnAStore.Controllers
+{
+	public class AccountController : Controller
+	{
+		private UserManager<User> _userManager;
+		private SignInManager<User> _signInManager;
+
+		public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel rvm)
+		{
+			if (true)
+			{
+
+			}
+		}
+	}
+}
---
+++ DnAStore/DnAStore/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnAStore.Models.ViewModels
+{
+	public class RegisterViewModel
+	{
+		[Required]
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; }
+
+		[Required]
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[StringLength(16, ErrorMessage = "The {0} must be between {2} and {1} characters long", MinimumLength = 6)]
+		public string Password { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm Password")]
+		[Compare("Password", ErrorMessage = "The passwords do not match")]
+		public string ConfirmPassword { get; set; }
+	}
+}
