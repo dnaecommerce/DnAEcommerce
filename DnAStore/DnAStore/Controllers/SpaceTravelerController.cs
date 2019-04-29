738b72fecdc1e45163771b30f736cec38a2af562
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-29 12:27:23 -0700

    Add SpaceTraveler view visible only to users with claim SpaceTravelCertified with value of true

---
+++ DnAStore/DnAStore/Controllers/SpaceTravelerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DnAStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DnAStore.Controllers
+{
+    public class SpaceTravelerController : Controller
+    {
+		private UserManager<User> _userManager;
+
+		public SpaceTravelerController(UserManager<User> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[Authorize(Policy = "SpaceTravelCertified")]
+        public async Task<IActionResult> SpaceTravelers()
+        {
+			//List<User> travelers = await _userManager.Users.ToListAsync();
+			//var result = travelers.Where(user => user.SpaceTravelCertified == true);
+			
+			return View();
+        }
+    }
+}
