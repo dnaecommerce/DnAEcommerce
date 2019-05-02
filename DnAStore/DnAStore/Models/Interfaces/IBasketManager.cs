62b66cbbc6bc7e1f130fc6d40c1ba8829210cc8b
Andrew Curtis <curtis.andrewm@gmail.com>
2019-05-02 10:06:39 -0700

    Add draft CRUD methods for BasketService

--- DnAStore/DnAStore/Models/Interfaces/IBasketManager.cs
+++ DnAStore/DnAStore/Models/Interfaces/IBasketManager.cs
@@ -10,6 +10,6 @@ namespace DnAStore.Models.Interfaces
 		//TODO Add method that gets product by ID from Products DB and adds that product to a Baskets DB? OR better, just add product ID as foreign key to Baskets DB
 
 
-		Task AddProductToBasket(Product product);
+		Task AddProductToBasket(int id); // Consider what other param(s) need to be included in addition to ID
 	}
 }
--- DnAStore/DnAStore/Models/Services/BasketService.cs
+++ DnAStore/DnAStore/Models/Services/BasketService.cs
@@ -2,22 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DnAStore.Data;
 using DnAStore.Models.Interfaces;
 
 namespace DnAStore.Models.Services
 {
 	public class BasketService : IBasketManager
 	{
-		private readonly BasketDBContext _context;
+		private readonly ProductDBContext _context;
 
-		public BasketService(BasketDBContext context)
+		public BasketService(ProductDBContext context)
 		{
 			_context = context;
 		}
 
+		public async Task CreateBasket(Basket basket)
+		{
+			_context.Add(basket);
+			await _context.SaveChangesAsync();
+		}
+		
+		//TODO Finish method for adding to basket
 		public Task AddProductToBasket(int id)
 		{
 			throw new NotImplementedException();
+		}
+
+		//TODO UpdateBasket method
+		public void UpdateBasket(int id, Basket basket)
+		{
+			if (id == Basket.ID)
+			{
+				_context.Update(basket);
+				_context.SaveChanges();
+			}
+		}
+
+		//TODO DeleteBasket method
+		public bool DeleteBasket(int id)
+		{
+			var basket = _context.Baskets.FirstOfDefault(b => b.ID == id);
+			if (basket != null)
+			{
+				_context.Remove(basket);
+				_context.SaveChanges();
+			}
+			return true;
 		}
 	}
 }
