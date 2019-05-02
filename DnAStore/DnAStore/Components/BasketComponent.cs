8342d6fa9c7f7f8b2e583c200c3b2de2b5223bfc
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-05-02 09:25:01 -0700

    Finished implementation of component for side basket

---
+++ DnAStore/DnAStore/Components/BasketComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DnAStore.Data;
+using Microsoft.AspNetCore.Identity;
+using DnAStore.Models;
+
+namespace DnAStore.Components
+{
+    public class BasketComponent : ViewComponent
+    {
+
+        private ProductDBContext _context;
+        private UserManager<User> _userManager;
+
+        public BasketComponent(ProductDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string username)
+        {
+            Basket basket = await _context.Baskets.FindByUser(username);
+            var basketItems = await _context.BasketItems.FindByBasketId(basket.Id);
+
+            return View(basketItems);
+        }
+    }
+}
