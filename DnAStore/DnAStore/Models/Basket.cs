ad06791406c23567a0bea2f1c6b1cbc307465066
Andrew Curtis <curtis.andrewm@gmail.com>
2019-05-02 09:08:49 -0700

    Add Basket.cs and BasketItem.cs

---
+++ DnAStore/DnAStore/Models/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnAStore.Models
+{
+	public class Basket
+	{
+		public int ID { get; set; }
+		public int UserID { get; set; }
+		public decimal Subtotal { get; set; }
+
+		// Nav property
+		public List<BasketItem> BasketItems { get; set; }
+	}
+}
