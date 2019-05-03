7682c864033c6f8e5c37ac66e50ed2c5497af979
Andrew Curtis <curtis.andrewm@gmail.com>
2019-05-03 10:22:09 -0700

    Start adding form and view model for AddToBasket button

---
+++ DnAStore/DnAStore/Models/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnAStore.Models.ViewModels
+{
+	public class DetailsViewModel
+	{
+		public string Username { get; set; }
+
+		public Product Product { get; set; }
+	}
+}
