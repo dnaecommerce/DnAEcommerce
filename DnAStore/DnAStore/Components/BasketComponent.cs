897b349d792ddc4043b6f066ea1588ebce8dec3b
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-05-02 19:52:06 -0700

    Added conditional incase basket hasn't been created yet.

--- DnAStore/DnAStore/Components/BasketComponent.cs
+++ DnAStore/DnAStore/Components/BasketComponent.cs
@@ -23,6 +23,7 @@ namespace DnAStore.Components
         public async Task<IViewComponentResult> InvokeAsync(string username)
         {
             Basket basket = await _context.Baskets.FindByUser(username);
+            if (basket == null) return View(new List<BasketItem>());
             var basketItems = await _context.BasketItems.FindAllByBasketId(basket.Id);
 
             return View(basketItems);
