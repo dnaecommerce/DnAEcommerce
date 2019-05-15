691a009750b83d0c0ad3ddd1e4c9fea330480841
Andrew Curtis <curtis.andrewm@gmail.com>
2019-05-14 19:17:16 -0700

    Add initial Dashboard Razor page and page model; add admin-only Dashboard link in _Layout

---
+++ DnAStore/DnAStore/Pages/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DnAStore.Pages.Admin
+{
+	[Authorize(Roles = "Admin")]
+    public class DashboardModel : PageModel
+    {
+		public void OnGet()
+        {
+
+        }
+    }
+}
