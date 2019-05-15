92ac365d6a227bb9ac25fc64b28a1965146e45b9
Andrew Curtis <curtis.andrewm@gmail.com>
2019-05-15 12:05:54 -0700

    Add user profile Razor Page with access limited to registered users; minor UI edits elsewhere

---
+++ DnAStore/DnAStore/Pages/User/Profile.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DnAStore.Pages.User
+{
+	[Authorize(Roles = "Member")]
+	public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
