c2c2f759a77721879644b28f29926246dcf6867f
Daniel Logerstedt <daniel.logerstedt@gmail.com>
2019-05-16 11:43:06 -0700

    Built Basket Getter Setter Tests

--- DnAStore/XUnitTest_DnAStore/UnitTests_Basket.cs
+++ DnAStore/XUnitTest_DnAStore/UnitTests_Basket.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using DnAStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace XUnitTest_DnAStore
 {
-    class UnitTests_Basket
+    public class UnitTests_Basket
     {
+        [Fact]
+        public static void TestGetID()
+        {
+            Basket basket = new Basket();
+            Assert.Equal(0, basket.ID);
+            basket.ID = 1;
+            Assert.Equal(1, basket.ID);
+        }
+
+        [Fact]
+        public static void TestGetSetUserName()
+        {
+            Basket basket = new Basket();
+            Assert.Null(basket.UserName);
+            basket.UserName = "TestUserName";
+            Assert.Equal("TestUserName", basket.UserName);
+        }
+
+        [Fact]
+        public static void TestGetSubtotal()
+        {
+            Basket basket = new Basket();
+            Assert.Equal(0, basket.Subtotal);
+            basket.Subtotal = 1.00m;
+            Assert.Equal(1.00m, basket.Subtotal);
+        }
+
+        [Fact]
+        public static void TestGetBasketItems()
+        {
+            Basket basket = new Basket();
+            Assert.Null(basket.BasketItems);
+            basket.BasketItems = new List<BasketItem>();
+            Assert.Empty(basket.BasketItems);
+        }
+
     }
 }
