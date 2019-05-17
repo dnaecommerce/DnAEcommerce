8b00461a99ed58845328e2d8dc49434faa549c88
Andrew Curtis <curtis.andrewm@gmail.com>
2019-05-16 17:23:49 -0700

    Add working feature for user to change first and last name

--- DnAStore/DnAStore/Pages/User/Profile.cshtml.cs
+++ DnAStore/DnAStore/Pages/User/Profile.cshtml.cs
@@ -3,17 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DnAStore.Models;
 
 namespace DnAStore.Pages.User
 {
 	[Authorize(Roles = "Member")]
 	public class ProfileModel : PageModel
     {
-        public void OnGet()
-        {
 
-        }
+		private UserManager<Models.User> _userManager;
+
+		[BindProperty]
+		public Models.User AppUser { get; set; } 
+
+
+		public ProfileModel(UserManager<Models.User> userManager)
+		{
+			_userManager = userManager;
+		}
+
+
+        public async Task OnGet()
+        {
+			string nameOfCurrentUser = User.Identity.Name;
+			AppUser = await _userManager.FindByNameAsync(nameOfCurrentUser);
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			string nameOfCurrentUser = User.Identity.Name;
+			var user = await _userManager.FindByNameAsync(nameOfCurrentUser);
+
+			user.FirstName = AppUser.FirstName;
+			user.LastName = AppUser.LastName;
+
+			await _userManager.UpdateAsync(user);
+
+			return Page();
+		}
     }
 }
